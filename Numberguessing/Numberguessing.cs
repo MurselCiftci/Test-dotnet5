560da7d3f26590dec72ffd5e881eb897b4384a74
MurselCiftci <mursel_ciftci@outlook.com>
2022-04-03 13:42:44 +0200

    1. Moved if statement for amount of guesses up to the first if statement in the while loop. 2. Added logic to the if statement checking for number of guesses that if you have the correct guess on 5th attempt, you don't lose the game 3. Added guessNumber - randomNumber integer 4. Added guessNumber - randomNumber int conversion to absolute number (positive number, removing the -) 5. Changed the if statement for youre close and added system.linq library for a function to check between a range of numbers

--- Numberguessing/Numberguessing.cs
+++ Numberguessing/Numberguessing.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Numberguessing
 {
@@ -9,37 +10,50 @@ namespace Numberguessing
             Random rnd = new Random();
             int randomNumber = rnd.Next(1, 20);
             int guesses = 0;
+            bool gameRunning = true;
+
             Console.WriteLine("Hello, this is a GIT test");
-
-            while (true)
+            while (gameRunning == true)
             {
-                Console.WriteLine("Guess the right number: ");
-                int guessNumber = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    Console.WriteLine("Guess the right number: ");
+                    
+                    int guessNumber = Convert.ToInt32(Console.ReadLine()); 
+                    int negativeNumberToPositive = guessNumber - randomNumber;
+                    int negativeNumberToPositiveConversion = Math.Abs(negativeNumberToPositive);
+                    int guessMinusRandom = guessNumber - randomNumber;
 
-                if (guessNumber - randomNumber < 5)
-                {
-                    Console.WriteLine("youre close");
-                }
+                    if (guessNumber != randomNumber && guesses >= 5)
+                    {
+                        Console.WriteLine("You have guessed too often! You failed");
+                        gameRunning = false;
+                        break;
+                    }
 
-                if (guessNumber > randomNumber)
-                {
-                    Console.WriteLine("Your estimate is too high");
-                    guesses++;
-                }
-                if (guessNumber < randomNumber)
-                {
-                    Console.WriteLine("Your estimate is too low");
-                    guesses++;
-                }
-                if (guesses == 5)
-                {
-                    Console.WriteLine("You have guessed too often! Try again!");
-                    Main(args);
-                }
-                if (guessNumber == randomNumber)
-                {
-                    Console.WriteLine("You have won!");
-                    Main(args);
+                    if (guessNumber != randomNumber)
+                    {
+                        if (Enumerable.Range(1,5).Contains(guessMinusRandom) || negativeNumberToPositiveConversion <= 5)
+                        {
+                            Console.WriteLine("youre close!");
+                        }
+                        if (guessNumber > randomNumber)
+                        {
+                            Console.WriteLine("Your estimate is too high");
+                            guesses++;
+                        }
+                        if (guessNumber < randomNumber)
+                        {
+                            Console.WriteLine("Your estimate is too low");
+                            guesses++;
+                        }
+                    }
+                    else 
+                    {
+                        Console.WriteLine("You have won!");
+                        gameRunning = false;
+                        break;
+                    }
                 }
             }
         }
